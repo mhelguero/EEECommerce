4812bbd24c34b6ef7310c346bf74c11d8919d3db
Miguel <miguelihelguero@gmail.com>
2024-09-17 13:06:04 -0500

    added corsconfig

--- eeecommerce_backend/src/main/java/com/revature/eeecommerce/util/CorsConfig.java
+++ eeecommerce_backend/src/main/java/com/revature/eeecommerce/util/CorsConfig.java
@@ -1,23 +1,20 @@
-//package com.revature.eeecommerce.util;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://18.118.75.83:5173")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                        .allowedHeaders("*");
-//            }
-//        };
-//    }
-//}
+package com.revature.eeecommerce.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://18.118.75.83:5173")  // Allow the specific origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true)  // Allow credentials (cookies, authorization headers)
+                .maxAge(3600);  // Cache the response for 1 hour
+    }
+}

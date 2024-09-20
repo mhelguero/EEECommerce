24b4526816842d1341a8bbc36d35189bf5ed8437
Miguel <miguelihelguero@gmail.com>
2024-09-20 14:54:07 -0500

    implemented getOrderItems

--- eeecommerce_frontend/src/components/UserOrder/UserOrders.tsx
+++ eeecommerce_frontend/src/components/UserOrder/UserOrders.tsx
@@ -11,7 +11,6 @@ const UserOrders: React.FC<Props> = ({ userId }: { userId: number | null }) => {
 
   const [userOrders, setUserOrders] = useState<Order[]>([]);
 
-
   // runs as soon as this Route is visited
   useEffect(() => {
     // arrow function to retrieve user info
@@ -19,17 +18,21 @@ const UserOrders: React.FC<Props> = ({ userId }: { userId: number | null }) => {
       try {
         console.log("user id in UserOrders.tsx: ", userId);
         const response = await axios.get(
-
           `http://3.144.166.99:8080/orders/user/${userId}`
-
         );
 
         // GET reseponse.data has same structure as UserType, so can assign directly to userOrders via setUserOrders()
         setUserOrders(response.data);
-        
+
         const userOrders = response.data;
         console.log("userOrders retrieved: ", userOrders);
-        
+        userOrders.map(async (order: { orderId: number }) => {
+          const orderItems = await getOrderItems(order.orderId);
+          console.log(
+            "order items for order " + order.orderId + ": ",
+            orderItems
+          );
+        });
       } catch (error) {
         console.error("Error during request:", error);
       }
@@ -41,8 +44,7 @@ const UserOrders: React.FC<Props> = ({ userId }: { userId: number | null }) => {
     }
   }, [userId]);
 
-
-  const getOrderItems = async(orderId: number) =>{
+  const getOrderItems = async (orderId: number) => {
     try {
       console.log("order id: ", orderId);
       const response = await axios.get(
@@ -62,24 +64,25 @@ const UserOrders: React.FC<Props> = ({ userId }: { userId: number | null }) => {
 
       // GET reseponse.data has same structure as UserType, so can assign directly to userOrders via setUserOrders()
       console.log("Order Item data: ", response.data);
+      return response.data;
     } catch (error) {
       console.error("Error during request:", error);
     }
-  }
+  };
   return (
     <>
       {/* display the retrieved user data as the content of this component*/}
       <h2>Profile</h2>
       <ul>
-
-         <li>Orders:</li>
-         {userOrders.map((order) => (
-           <li key={order.orderId}>
-             Order ID: {order.orderId}
-             Time: {order.date}
-           </li>
-         ))}    
-       </ul>
+        <li>Orders:</li>
+        {userOrders.map((order) => (
+          <li key={order.orderId}>
+            Order ID: {order.orderId}
+            <br />
+            Time: {order.date}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
